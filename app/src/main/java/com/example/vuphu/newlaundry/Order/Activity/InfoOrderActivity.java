0faf61a6e5d1ec4a239357b2fb1cb7eb1577ae29
phuong vu <phuongtinhbien12@gmail.com>
2018-09-12 00:27:11 +0700

    add activity - bag

--- app/src/main/java/com/example/vuphu/newlaundry/Order/Activity/InfoOrderActivity.java
+++ app/src/main/java/com/example/vuphu/newlaundry/Order/Activity/InfoOrderActivity.java
@@ -1,14 +1,19 @@
 package com.example.vuphu.newlaundry.Order.Activity;
 
 import android.annotation.TargetApi;
+import android.content.Intent;
 import android.os.Build;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.widget.Button;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 
+import com.example.vuphu.newlaundry.ItemListDialogFragment;
 import com.example.vuphu.newlaundry.Order.Adapter.ListClothesAdapter;
 import com.example.vuphu.newlaundry.Order.OBOrderDetail;
 import com.example.vuphu.newlaundry.Payment.OBPayment;
@@ -19,18 +24,24 @@ import com.example.vuphu.newlaundry.Utils.Util;
 import com.github.florent37.androidslidr.Slidr;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
-public class InfoOrderActivity extends AppCompatActivity {
+public class InfoOrderActivity extends AppCompatActivity implements ItemListDialogFragment.Listener {
 
-
+    private static final String TYPE_LIST_PAYMENT = "PM";
+    ArrayList<String> paymentList = new ArrayList<>();
     private Toolbar toolbar;
     private RecyclerView listClothes;
     private Slidr deliveryDate;
     private TextView deliveryYourChoice;
+    private Button checkOut;
 
     private ListClothesAdapter adapter;
     private List<OBOrderDetail> orderDetailList = new ArrayList<>();
+
+    private TextView paymentValue, promotionValue, noteValue;
+    private LinearLayout payment, promotion, note;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -40,6 +51,7 @@ public class InfoOrderActivity extends AppCompatActivity {
 
     }
     private void init() {
+        checkOut = findViewById(R.id.btn_check_out);
         listClothes = findViewById(R.id.list_prepare_order_clothes);
 
         LinearLayoutManager linearLayoutManager = new LinearLayoutManager(getApplicationContext());
@@ -51,14 +63,6 @@ public class InfoOrderActivity extends AppCompatActivity {
         listClothes.setLayoutManager(linearLayoutManager2);
         listClothes.setHasFixedSize(true);
 
-        List<OBPayment> paymentList = new ArrayList<>();
-        OBPayment itemPayment = new OBPayment();
-        itemPayment.setName("Money Cash");
-        itemPayment.setDesc("A lot type of payment for you choose");
-        paymentList.add(itemPayment);
-        paymentList.add(itemPayment);
-        paymentList.add(itemPayment);
-
         orderDetailList = new ArrayList<>();
         prepareList();
         adapter = new ListClothesAdapter(this, orderDetailList);
@@ -69,7 +73,29 @@ public class InfoOrderActivity extends AppCompatActivity {
         deliveryYourChoice = findViewById(R.id.prepare_order_devlivery_3_name);
         setDeliveryYourChoice();
 
+        checkOut.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(getApplicationContext(), FinalOrderActivity.class));
+            }
+        });
 
+        paymentValue = findViewById(R.id.item_prepare_order_payment);
+        promotionValue = findViewById(R.id.item_prepare_order_promotion);
+        noteValue = findViewById(R.id.item_prepare_order_note);
+
+        payment = findViewById(R.id.check_out_payment);
+        promotion = findViewById(R.id.check_out_promotion);
+        note = findViewById(R.id.check_out_note);
+
+        paymentList.addAll(Arrays.asList(getResources().getStringArray(R.array.arr_payment)));
+        payment.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                ItemListDialogFragment bottomSheetDialogFragment = ItemListDialogFragment.newInstance(TYPE_LIST_PAYMENT,paymentList);
+                bottomSheetDialogFragment.show(getSupportFragmentManager(), bottomSheetDialogFragment.getTag());
+            }
+        });
 
 
 
@@ -120,5 +146,11 @@ public class InfoOrderActivity extends AppCompatActivity {
                 detail.setCategory("Category Type 2");
             orderDetailList.add(detail);
         }
+    }
+
+    @Override
+    public void onItemClicked(String type, int position) {
+        paymentValue.setText(paymentList.get(position));
+
     }
 }
