e6d2a3c08d4224203b60a354f7830e127e5f0145
phuong vu <phuongtinhbien12@gmail.com>
2018-09-20 23:39:59 +0700

    add graphql

---
+++ app/src/main/java/com/example/vuphu/newlaundry/Graphql/GraphqlClient.java
@@ -0,0 +1,24 @@
+package com.example.vuphu.newlaundry.Graphql;
+
+import com.apollographql.apollo.ApolloClient;
+
+import okhttp3.OkHttpClient;
+
+public class GraphqlClient {
+
+    private static final String BASE_URL = "http://localhost:3000/graphql";
+
+    private static OkHttpClient okHttpClient = new OkHttpClient.Builder().build();
+
+    private static ApolloClient apolloClient = ApolloClient.builder()
+            .serverUrl(BASE_URL)
+            .okHttpClient(okHttpClient)
+            .build();
+
+    public static ApolloClient getApolloClient() {
+        return apolloClient;
+    }
+
+
+
+}
