6a54a0e320fa02de41e17c0cb05fac497be2ce11
phuong vu <phuongtinhbien12@gmail.com>
2018-10-23 11:52:50 +0700

    Merge branch 'master' of E:\CamNangHoaHoc2\NewLaundry2 with conflicts.

---
+++ app/src/main/java/com/example/vuphu/newlaundry/ChooseUnitActivity.java
@@ -0,0 +1,13 @@
+package com.example.vuphu.newlaundry;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ChooseUnitActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_choose_unit);
+    }
+}
